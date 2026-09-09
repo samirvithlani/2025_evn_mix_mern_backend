0628e666d2456dca625fd0fa233372de5e87e829
samirvithlani <samir.vithlani83955@gmail.com>
2026-09-09 17:38:51 +0530

    get api

--- app.js
+++ app.js
@@ -3,6 +3,25 @@ const express = require("express")
 const app = express()
 
 
+//GET API
+//localhost:3000/test
+//request ,response
+
+app.get("/test",(req,res)=>{
+    console.log("test api called...")
+    res.send("test api called...")
+})
+
+const user = {id:1,name:"amit",age:23}
+
+//localhost:3000/user
+app.get("/user",(req,res)=>{
+
+    res.json({message:"user fetched",data:user})
+})
+
+
+
 
 const PORT = 3000
 
