9ebaa2e3de192b6ccf0f7d81d49068bf0925f478
samirvithlani <samir.vithlani83955@gmail.com>
2026-09-18 17:15:04 +0530

    findById

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,27 +5,39 @@
 //     res.json({message:"user fetched",data:user})
 // })
 
-const userModel = require("../models/UserModel")
+const userModel = require("../models/UserModel");
 
-const getUsers = async(req,res)=>{
-    
-        //db.users.find()
-        const users = await userModel.find() 
+const getUsers = async (req, res) => {
+  //db.users.find()
+  const users = await userModel.find();
 
-        //response send
-        res.json({message:"user fetched",data:users})
-}
+  //response send
+  res.json({ message: "user fetched", data: users });
+};
 
-const  getUserById = (req,res)=>{
-    //id --> req ?
-    const id = req.params.id;
+const getUserById = async (req, res) => {
+  //id --> req ?
+  //db.users.find({_id:"6aaa83e3b399a3b5eaada9e1"})
+  //db.users.find({_id:req.params.id})
+  //userModel.find({_id:req.params.id})
+  //find function return type  -> []
+
+  const foundUser = await userModel.findById(req.params.id);
+  if (foundUser) {
     res.json({
-        message:"user by id",
-        data:id
-    })
-}
+      message: "user found",
+      data: foundUser,
+    });
+  }
+  else{
+    res.json({
+      message: "user not found",
+      
+    });
+  }
+};
 
-module.exports ={
-    getUsers,
-    getUserById
-}
+module.exports = {
+  getUsers,
+  getUserById,
+};
